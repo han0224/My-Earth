6f1bbf490979366076f8654dd2d2f3bdcbd21399
han0224 <dbn0224@gmail.com>
2023-09-29 16:14:59 +0900

    fix: cors 수정

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://my-earth.vercel.app/"],
+    origin: ["http://localhost:3000", "https://my-earth.vercel.app"],
     methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
   })
